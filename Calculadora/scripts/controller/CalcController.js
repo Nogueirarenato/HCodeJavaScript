a9e8c58344e0f6bcdd3a1cccf2800125dac6d722
unknown <renatonogueirasenai@gmail.com>
2021-02-12 21:56:06 -0200

    up

--- Calculadora/scripts/controller/CalcController.js
+++ Calculadora/scripts/controller/CalcController.js
@@ -1,7 +1,41 @@
 class CalcController {
 
     constructor() {
-        var displayCalc = '0';
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._hourEl = document.querySelector("#hora");
+        this._currentDate;
+        this.initialize();
     }
+
+    initialize() {
+       
+        this._dateEl.innerHTML = "27/05"
+        this._hourEl.innerHTML = "10:15"
+    }
+
+
+
+    get displayCalc() {
+        return this._displayCalEl.innerHTML;
+    }
+
+    set displayCalc(value) {
+        this._displayCalcEl.innerHTML = value;
+    }
+
+    get currentDate() {
+        return new Date;
+    }
+
+    set currentDate(value) {
+        this._currentDate = value;
+    }
+
+
+
+
+
+
 
 }
